cc37d1ec31ddfb2cf78ecfd18595e8de741a9c33
evaranshark <evarancorp@gmail.com>
2023-07-27 19:58:02 +0300

    Code cleanup

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -9,6 +9,7 @@ import 'package:omega_app/constants.dart';
 import 'package:omega_app/helpers.dart';
 import 'package:omega_app/widgets/omega_text_button.dart';
 
+import 'widgets/link_text.dart';
 import 'widgets/omega_form_field.dart';
 
 class LoginForm extends StatefulWidget {
@@ -52,139 +53,126 @@ class _LoginFormState extends State<LoginForm> {
     return ConstrainedBox(
       constraints: const BoxConstraints(
         maxWidth: 500,
+        minWidth: 200,
       ),
-      child: ClipRect(
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.75),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: LayoutBuilder(builder: (context, constraints) {
-                        return Text(
-                          "Вход",
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayMedium
-                              ?.copyWith(
-                                color: AppColors.textH,
-                                fontSize:
-                                    _calculateHFontSize(constraints.maxWidth),
-                              ),
-                        );
-                      }),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: LayoutBuilder(builder: (context, constraints) {
-                        return Text(
-                          "Введите данные, чтобы войти в личный кабинет.",
-                          style:
-                              Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                    color: AppColors.textP,
-                                    fontSize: Helpers.constraintWidthValue(
-                                      constraintWidth: constraints.maxWidth,
-                                      values: [(400, 15)],
-                                      defaultValue: 16,
-                                    ),
-                                  ),
-                        );
-                      }),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    OmegaFormField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        label: Text("E-mail"),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Пожалуйста, введите E-mail";
-                        }
-                        if (!EmailValidator.validate(value)) {
-                          return " Некорректный e-mail";
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    OmegaFormField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        label: Text("Пароль"),
-                      ),
-                      validator: (value) {
-                        if ((value == null || value.isEmpty)) {
-                          return 'Пожалуйста, введите пароль';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: OmegaTextButton.underlined(
-                        onPressed: widget.onForgotPassword ?? () {},
-                        child: const Text("Забыли пароль?"),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    ElevatedButton(
-                        onPressed: onPressed,
-                        child: const Text(
-                          "Войти",
-                        )),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    const _LoginFormDivider(),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    const SocialLoginRow(),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Center(
-                      child: Text.rich(
-                        TextSpan(text: "Ещё нет аккаунта? ", children: [
-                          WidgetSpan(
-                              alignment: PlaceholderAlignment.baseline,
-                              baseline: TextBaseline.alphabetic,
-                              // Alternatively we can use [LinkText] widget as a child
-                              child: OmegaTextButton(
-                                onPressed: widget.onRegister ?? () {},
-                                child: const Text("Зарегистрируйтесь"),
-                              )),
-                        ]),
-                      ),
-                    ),
-                  ],
+      child: _LoginFormDecoration(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    return Text(
+                      "Вход",
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayMedium
+                          ?.copyWith(
+                            color: AppColors.textH,
+                            fontSize: _calculateHFontSize(constraints.maxWidth),
+                          ),
+                    );
+                  }),
                 ),
-              ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    return Text(
+                      "Введите данные, чтобы войти в личный кабинет.",
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: AppColors.textP,
+                            fontSize: Helpers.constraintWidthValue(
+                              constraintWidth: constraints.maxWidth,
+                              values: [(400, 15)],
+                              defaultValue: 16,
+                            ),
+                          ),
+                    );
+                  }),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                OmegaFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    label: Text("E-mail"),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Пожалуйста, введите E-mail";
+                    }
+                    if (!EmailValidator.validate(value)) {
+                      return "Некорректный e-mail";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                OmegaFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    label: Text("Пароль"),
+                  ),
+                  validator: (value) {
+                    if ((value == null || value.isEmpty)) {
+                      return 'Пожалуйста, введите пароль';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: OmegaTextButton.underlined(
+                    onPressed: widget.onForgotPassword ?? () {},
+                    child: const Text("Забыли пароль?"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                ElevatedButton(
+                    onPressed: onPressed,
+                    child: const Text(
+                      "Войти",
+                    )),
+                const SizedBox(
+                  height: 25,
+                ),
+                const _LoginFormDivider(),
+                const SizedBox(
+                  height: 25,
+                ),
+                const SocialLoginRow(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Text.rich(
+                    TextSpan(text: "Ещё нет аккаунта? ", children: [
+                      WidgetSpan(
+                          child: LinkText(
+                        onPressed: widget.onRegister ?? () {},
+                        text: "Зарегистрируйтесь",
+                      )),
+                    ]),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -203,6 +191,30 @@ class _LoginFormState extends State<LoginForm> {
       constraintWidth: maxWidth,
       values: [(400, 39)],
       defaultValue: 50,
+    );
+  }
+}
+
+class _LoginFormDecoration extends StatelessWidget {
+  final Widget child;
+  const _LoginFormDecoration({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.75),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: child,
+        ),
+      ),
     );
   }
 }
